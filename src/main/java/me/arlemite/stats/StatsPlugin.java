f5192b7ab7742de0772841899c09c1a9fca3baad
Arle <nortoynozio19@gmail.com>
2026-04-21 15:42:55 +0500

    Update StatsPlugin.java

--- src/main/java/me/arlemite/stats/StatsPlugin.java
+++ src/main/java/me/arlemite/stats/StatsPlugin.java
@@ -1,5 +1,7 @@
 package me.arlemite.stats;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class StatsPlugin extends JavaPlugin {
@@ -16,7 +18,32 @@ public class StatsPlugin extends JavaPlugin {
         database.connect();
 
         getCommand("stats").setExecutor(new StatsCommand());
+        getCommand("tonstats").setExecutor(new TonStatsCommand());
+        
         getServer().getPluginManager().registerEvents(new StatsListener(), this);
+
+        // Авто-синхронизация онлайн игроков в БД
+        int intervalSeconds = getConfig().getInt("update-interval-seconds", 300);
+        Bukkit.getScheduler().runTaskTimerAsynchronously(this, () -> {
+            for (Player p : Bukkit.getOnlinePlayers()) {
+                int msgs = StatsListener.popMessages(p.getUniqueId());
+                PlayerStatsData data = StatsManager.collect(p, msgs);
+                database.savePlayerStatsAsync(data);
+            }
+        }, intervalSeconds * 20L, intervalSeconds * 20L);
+    }
+
+    @Override
+    public void onDisable() {
+        // Финальное сохранение при выключении сервера
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            int msgs = StatsListener.popMessages(p.getUniqueId());
+            PlayerStatsData data = StatsManager.collect(p, msgs);
+            database.savePlayerStatsAsync(data); // База закроется чуть позже
+        }
+        if (database != null) {
+            database.close();
+        }
     }
 
     public static StatsPlugin get() {
