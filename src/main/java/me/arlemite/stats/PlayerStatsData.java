6d111ef0c7dcc6ae60009bb8fdaa774e6ee7ab05
Arle <nortoynozio19@gmail.com>
2026-04-21 15:40:31 +0500

    Create PlayerStatsData.java

---
+++ src/main/java/me/arlemite/stats/PlayerStatsData.java
@@ -0,0 +1,13 @@
+package me.arlemite.stats;
+
+public class PlayerStatsData {
+    public String uuid;
+    public String name;
+    public long playtime;
+    public int advancements;
+    public int kills, mobKills, deaths;
+    public int blocksBroken, blocksPlaced, itemsCrafted;
+    public int walkedM, swumM;
+    public int sessionMessages;
+}
+
